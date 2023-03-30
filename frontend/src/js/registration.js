5169144f0b942e66d065fd9343a152a2f2906e4b
khaimindm <78298085+khaimindm@users.noreply.github.com>
2023-03-30 16:05:41 +0400

    30.03.23

---
+++ frontend/src/js/registration.js
@@ -0,0 +1,18 @@
+﻿document.getElementById('main-form').addEventListener("submit", checkForm);
+
+function todayDate() {
+let today = new Date();
+
+let yyyy = today.getFullYear();
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let dd = String(today.getDate()).padStart(2, '0');
+
+let result = yyyy + '-' + mm + '-' + dd;
+document.getElementById('birthday').max = result;
+console.log('Дата: ' + document.getElementById('birthday').max);
+}
+
+todayDate();
+
+function checkForm() {
+}

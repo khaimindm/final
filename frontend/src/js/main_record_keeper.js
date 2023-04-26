83fa958a880a6ab16997d58159ee29dcfe8bd051
khaimindm <khaimindm@gmail.com>
2023-04-26 12:10:30 +0000

    26.04.23

--- frontend/src/js/main_record_keeper.js
+++ frontend/src/js/main_record_keeper.js
@@ -3,3 +3,32 @@ var deleteModal = document.getElementById("deleteModal");
 
 var addSpecBtn = document.getElementById("addSpecBtn");
 var deleteSpecBtn = document.getElementById("deleteSpecBtn");
+
+var addSpan = document.getElementsByClassName("addClose")[0];
+var deleteSpan = document.getElementsByClassName("deleteClose")[0];
+
+addSpecBtn.onclick = function() {
+    addModal.style.display = "block";
+}
+
+deleteSpecBtn.onclick = function() {
+    deleteModal.style.display = "block";
+}
+
+addSpan.onclick = function() {
+    addModal.style.display = "none";
+}
+
+deleteSpan.onclick = function() {
+    deleteModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == addModal) {
+        addModal.style.display = "none";
+    }
+
+    if (event.target == deleteModal) {
+        deleteModal.style.display = "none"
+    }
+}

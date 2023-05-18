5dc5c2c39842526463c7b593182d5967d3c7d782
khaimindm <khaimindm@gmail.com>
2023-05-18 12:10:12 +0000

    18.05.23

--- frontend/src/js/main_specialist.js
+++ frontend/src/js/main_specialist.js
@@ -2,9 +2,17 @@ document.getElementById("elForMainTable").addEventListener('click', userBtn);
 
 var modal = document.getElementById("doctorsAppointmentModal");
 
-function userBtn(event) {    
-    console.log(event.target)    
-    modal.style.display = "block";
+var userEl = "";
+var userId = "";
+
+function userBtn(event) {
+    userEl = event.target;
+
+    if (userEl.nodeName == "SPAN") {
+        modal.style.display = "block";
+        userId = userEl.getAttribute("id");        
+        document.getElementById('modalHeader').innerHTML = userEl.innerHTML + " User Id: " + userId;
+    }
 }
 
 var span = document.getElementsByClassName("close")[0];

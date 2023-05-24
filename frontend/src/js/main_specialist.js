4673b842642c786c08656c873241086866602ecd
khaimindm <khaimindm@gmail.com>
2023-05-24 14:32:22 +0000

    24.05.2023

--- frontend/src/js/main_specialist.js
+++ frontend/src/js/main_specialist.js
@@ -5,6 +5,11 @@ var modal = document.getElementById("doctorsAppointmentModal");
 var userEl = "";
 var userId = "";
 
+var cancelBtn = document.getElementById("cancelBtn");
+var applyBtn = document.getElementById("applyBtn");
+
+var errorMessage = "";
+
 function userBtn(event) {
     userEl = event.target;
 
@@ -25,4 +30,14 @@ window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
+}
+
+cancelBtn.onclick = function() {
+    modal.style.display = "none";
+    hideMessage();
+}
+
+function hideMessage() {
+    document.getElementById("message").innerHTML = "";
+    document.getElementById("message").style.visibility = "hidden";
 }

ae950e0db526b0889e80fbe3ee95f7bb24f81695
khaimindm <khaimindm@gmail.com>
2023-05-26 12:07:22 +0000

    26.05.2023

--- frontend/src/js/main_specialist.js
+++ frontend/src/js/main_specialist.js
@@ -1,4 +1,5 @@
 document.getElementById("elForMainTable").addEventListener('click', userBtn);
+document.getElementById("makeAnAppointmentBtn").addEventListener('click', makeAnAppointmentBtn);
 
 var modal = document.getElementById("doctorsAppointmentModal");
 
@@ -8,27 +9,36 @@ var userId = "";
 var cancelBtn = document.getElementById("cancelBtn");
 var applyBtn = document.getElementById("applyBtn");
 
+var doctorsAppointmentModalForm = document.getElementById("doctorsAppointmentModalForm");
+
 var errorMessage = "";
 
 function userBtn(event) {
     userEl = event.target;
 
     if (userEl.nodeName == "SPAN") {
+        clearAreas();        
         modal.style.display = "block";
         userId = userEl.getAttribute("id");        
         document.getElementById('modalHeader').innerHTML = userEl.innerHTML + " User Id: " + userId;
     }
 }
 
+function makeAnAppointmentBtn(event) {
+    document.location = "appointment.html";
+}
+
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
     modal.style.display = "none";
+    hideMessage();        
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      hideMessage();
     }
 }
 
@@ -40,4 +50,27 @@ cancelBtn.onclick = function() {
 function hideMessage() {
     document.getElementById("message").innerHTML = "";
     document.getElementById("message").style.visibility = "hidden";
+}
+
+function clearAreas() {
+    doctorsAppointmentModalForm.medicalHistory.value = "";
+    doctorsAppointmentModalForm.diagn.value = "";    
+}
+
+
+document.getElementById("doctorsAppointmentModalForm").addEventListener("submit", checkDoctorsAppointmentModalForm);
+
+function checkDoctorsAppointmentModalForm(event) {
+    event.preventDefault();
+
+    var medicalHistory = doctorsAppointmentModalForm.medicalHistory.value;
+    var diagn = doctorsAppointmentModalForm.diagn.value;
+
+    if (medicalHistory == "" || diagn == "") {
+        errorMessage = "Заполните все поля";
+        document.getElementById("message").innerHTML = errorMessage;
+        document.getElementById("message").style.visibility = "visible";
+    } else {
+        hideMessage();
+    }
 }

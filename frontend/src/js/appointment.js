a0cf43bff7553a12bbbd84a2729595b9ed8864b9
khaimindm <khaimindm@gmail.com>
2023-06-02 11:50:52 +0000

    02.06.2023

--- frontend/src/js/appointment.js
+++ frontend/src/js/appointment.js
@@ -4,12 +4,40 @@ var btn = document.getElementById("applyBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
+var date = document.getElementById("date");
+
+var selectedDate;
+
+document.addEventListener("DOMContentLoaded", function(){
+    var d = new Date();
+    
+    var day = d.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    var month = d.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    var year = d.getFullYear();
+    var name_input = document.getElementById("date");    
+    var currentDate = year + "-" + month + "-" + day;
+    name_input.value = currentDate;
+    selectedDate = currentDate;
+});
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
 span.onclick = function() {
     modal.style.display = "none";
+}
+
+date.onchange = function() {
+    selectedDate = date.value;    
 }
 
 window.onclick = function(event) {
